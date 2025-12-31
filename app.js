635a82445a7a5f59cf6fd519b8313c5f399de312
Torgbe-Desmond <123torgbe@gmail.com>
2025-12-31 02:26:12 +0000

    fixed cors issue

--- app.js
+++ app.js
@@ -14,7 +14,10 @@ app.use(express.json());
 
 app.use(
   cors({
-    origin: "http://localhost:3001",
+    origin: [
+      "http://localhost:3001",
+      "https://breath-analysis-frontend.vercel.app",
+    ],
     credentials: true,
   })
 );
